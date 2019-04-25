1c98d1b29ebe7e6be9063c69ce27d46f3037f247
Jimachine <jimmy_onion@hotmail.com>
2019-04-25 09:53:27 +0100

    initial commit

---
+++ Complementary DNA/DnaStrand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complementary_DNA
+{
+    public class DnaStrand
+    {
+        public static string MakeComplement(string dna)
+        {
+            string output = null;
+
+            foreach (char c in dna)
+            {
+                if (c == 'A')
+                {
+                    output += 'T';
+                }
+                else if (c == 'T')
+                {
+                    output += 'A';
+                }
+                else if (c == 'C')
+                {
+                    output += 'G';
+                }
+                else if (c == 'G')
+                {
+                    output += 'C';
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ Complementary DNA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complementary_DNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DnaStrand.MakeComplement("ATTGC");
+        }
+    }
+}
